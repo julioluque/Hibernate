a20134f71f3abe0a3a26681d77f8c50976f11bd8
Julio Luque <julio.a.luque@gmail.com>
2020-05-14 02:44:59 -0300

    Mapeo ORM y conexiones estables MySql & Dbeaver -> Fixear autoincrementales de Oracle y SqlServer

--- src/ar/jluque/hibernate/JdbcHibernate.java
+++ src/ar/jluque/hibernate/JdbcHibernate.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class JdbcHibernate {
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException{
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
 		String urlMySql = "jdbc:mysql://localhost:3306/springhibernate?useSSL=false";
 		String userMySql = "root";
@@ -20,29 +20,38 @@ public class JdbcHibernate {
 		String userSqlServer = "delfina";
 		String passwordSqlServer = "delfina";
 
+//		String urlPostgreSQL = "jdbc:postgresql://localhost:5432/springhibernate";
+//		String userPostgreSQL = "delfina";
+//		String passwordPostgreSQL= "delfina";
+
 		try {
 
-			System.out.println(".......... INTENTANDO CONECTAR A : " + urlMySql);
-			Connection conexionMySql = DriverManager.getConnection(urlMySql, userMySql, passwordMySql);
-			System.out.println(">>>>>>>>>> CONEXION EXITOSA!");
+//			MYSQL
+//			System.out.println(".......... INTENTANDO CONECTAR A : " + urlMySql);
+//			Connection conexionMySql = DriverManager.getConnection(urlMySql, userMySql, passwordMySql);
+//			System.out.println(">>>>>>>>>> CONEXION EXITOSA! Tambien a DBeaver");
+//			System.out.println("\n..Transacciones..\n");
+//			System.out.println(".......... CERRANDO LA CONEXION");
+//			conexionMySql.close();
+//			System.out.println("<<<<<<<<<< SE CERRO LA CONEXION!" + urlMySql);
 
+//			ORACLE
 			System.out.println(".......... INTENTANDO CONECTAR A : " + urlOracle);
 			Connection conexionOracle = DriverManager.getConnection(urlOracle, userOracle, passwordOracle);
 			System.out.println(">>>>>>>>>> CONEXION EXITOSA!");
-			
-			System.out.println(".......... INTENTANDO CONECTAR A : " + urlSqlServer);
-			Connection conexionSqlServer = DriverManager.getConnection(urlSqlServer, userSqlServer, passwordSqlServer);
-			System.out.println(">>>>>>>>>> CONEXION EXITOSA!");
-
-			System.out.println("\n..Transacciones..\n..Transacciones..\n");
-
-			System.out.println(".......... CERRANDO LA CONEXIONES : ");
-			conexionMySql.close();
-			System.out.println("<<<<<<<<<< SE CERRO LA CONEXION!" + urlMySql);
+			System.out.println("\n..Transacciones..\n");
+			System.out.println(".......... CERRANDO LA CONEXION");
 			conexionOracle.close();
 			System.out.println("<<<<<<<<<< SE CERRO LA CONEXION!" + urlOracle);
-			conexionSqlServer.close();
-			System.out.println("<<<<<<<<<< SE CERRO LA CONEXION!" + urlSqlServer);
+
+//			SQLSERVER
+//			System.out.println(".......... INTENTANDO CONECTAR A : " + urlSqlServer);
+//			Connection conexionSqlServer = DriverManager.getConnection(urlSqlServer, userSqlServer, passwordSqlServer);
+//			System.out.println(">>>>>>>>>> CONEXION EXITOSA!");
+//			System.out.println("\n..Transacciones..\n");
+//			System.out.println(".......... CERRANDO LA CONEXION");
+//			conexionSqlServer.close();
+//			System.out.println("<<<<<<<<<< SE CERRO LA CONEXION!" + urlSqlServer);
 
 		} catch (Exception e) {
 			System.out.println("XXXXXXXXXX No se pudo establecer conexion con alguna base de datos ");
---
+++ src/ar/jluque/hibernateorm/MainClientes.java
@@ -0,0 +1,45 @@
+package ar.jluque.hibernateorm;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class MainClientes {
+
+	public static void main(String[] args) {
+
+//		1 -> CREAMOS SESSION FACTORY
+		SessionFactory miFactory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Clientes.class)
+				.buildSessionFactory();
+
+//		2 -> CREAMOS OBJETO TIPO SESSION
+		Session miSession = miFactory.openSession();
+
+		try {
+			
+//		3 -> CREAMOS OBJETO CLIENTE
+			Clientes cliente1 = new Clientes("Julio", "Luque", "Ciudadela");
+			
+//		4 -> EJECUTAMOS TRANSACCION SQL
+			miSession.beginTransaction();
+			
+//		5 -> GUARDAMOS EN BASE DE DATOS
+			miSession.save(cliente1);
+			
+//		6 -> COMMIT / ROLLBACK 
+			miSession.getTransaction().commit();
+			
+			System.out.println("++ Registro insertado en base de datos...");
+		
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+		} finally {
+			miSession.close();
+		}
+
+
+	}
+
+}
